dd0109382df6467870ee659352b31d62788bd1db
Thomas Jebson <ThomasJebbo@gmail.com>
2024-10-08 18:06:13 +0100

    major tweaks to game loop and structure

--- AS1 - Bouncing Balls - Visual Studio Project/AS1 - Bouncing Balls/main.cpp
+++ AS1 - Bouncing Balls - Visual Studio Project/AS1 - Bouncing Balls/main.cpp
@@ -9,10 +9,23 @@
 #define END_MENU "end menu"
 #define WINDOW_SIZE {1000, 800} // Actual Game Size {600, 400}
 
+#define ENTER_KEY 58
+#define ESCAPE_KEY 36
+
 using namespace std;
 
-string current_component = MAIN_MENU;
-int score = 0;
+sf::Color pick_ball_color() {
+    int randomly_chosen_color = rand() % 3;
+    if (randomly_chosen_color == 0) {
+        return sf::Color::Blue;
+    }
+    else if (randomly_chosen_color == 1) {
+        return sf::Color::Yellow;
+    }
+    else {
+        return sf::Color::Red;
+    }
+}
 
 class Ball {
 };
@@ -41,10 +54,12 @@ public:
     }
 
     void update(sf::RenderWindow& window) {
-        sf::Vector2i internal_position = mouse.getPosition(window);
+        sf::Vector2i internal_position = mouse.getPosition(
+            window);
         position[0] = internal_position.x;
         position[1] = internal_position.y;
-        left_button_pressed = mouse.isButtonPressed(sf::Mouse::Button::Left);
+        left_button_pressed = mouse.isButtonPressed(
+            sf::Mouse::Button::Left);
     }
 
     int* get_position() {
@@ -54,34 +69,86 @@ public:
     bool get_left_button_pressed() {
         return left_button_pressed;
     }
+
+    void set_left_button_pressed(bool value) {
+        left_button_pressed = value;
+    }
 };
 
-string main_menu(sf::RenderWindow & window) {
-    int score = 0;
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    window.draw(shape);
-    return MAIN_MENU;
-}
+class MainMenu {
+public:
+    MainMenu() {
+    }
 
-string level_one(sf::RenderWindow & window) {
-    return LEVEL_ONE;
-}
+    string run_menu(sf::RenderWindow& window) {
+        int score = 0;
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(pick_ball_color());
+        window.draw(shape);
+        return MAIN_MENU;
+    }
 
-string level_two(sf::RenderWindow & window) {
-    return LEVEL_TWO;
-}
+};
 
-string end_menu(sf::RenderWindow & window) {
-    return END_MENU;
-}
+class LevelOne {
+public:
+    LevelOne() {
+    }
+
+    string run_menu(sf::RenderWindow& window) {
+        return LEVEL_ONE;
+    }
+
+};
+
+class LevelTwo {
+public:
+    LevelTwo() {
+    }
+
+    string run_menu(sf::RenderWindow& window) {
+        return LEVEL_TWO;
+    }
+};
+
+class EndMenu {
+public:
+    EndMenu() {
+    }
+
+    string run_menu(sf::RenderWindow& window) {
+        return END_MENU;
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE), "Bouncing Balls!");
-    window.setVerticalSyncEnabled(true);
+    // setup local variables
+    string menu_navigation[] = { MAIN_MENU, MAIN_MENU };
+    /*
+    * The Menu Navigation System, created in the variable menu_navigation, allows us
+    * to easily move between the current menu and the previous menu - so that the
+    * player can return to the main menu at any time or a pause menu and the game can
+    * "remember" what was happening befvore this switch occured. In a larger game
+    * the array could be larger as there are more menus to navigate through,
+    * but I am keeping this relatively simple and small.
+    */
+    int score = 0;
+    string next_component;
 
+    // setup game componentry
     Mouse mouse = Mouse();
+
+    // setup game levels
+    MainMenu main_menu = MainMenu();
+    LevelOne level_one = LevelOne();
+    LevelTwo level_two = LevelTwo();
+    EndMenu end_menu = EndMenu();
+
+    // create window (do last so not unresponsive whilst the game loads)
+    sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE), "Bouncing Balls!");
+    window.setVerticalSyncEnabled(true); // vsync is our frame limiter here.
+    // No need for clocks!
 
     while (window.isOpen())
     {
@@ -91,28 +158,47 @@ int main()
             if (event.type == sf::Event::Closed) {
                 window.close();
                 break;
+            } else if(event.type == sf::Event::KeyPressed) {
+                if (event.key.code == ENTER_KEY) {
+                    mouse.set_left_button_pressed(true);
+                }
+                else if (event.key.code == ESCAPE_KEY) {
+                    string current_menu = menu_navigation[0];
+                    string menu_navigation[] = { MAIN_MENU, current_menu };
+                }
             }
         }
 
         window.clear();
 
-        cout << mouse.get_left_button_pressed() << endl;
-
-        if (current_component == MAIN_MENU) {
-            current_component = main_menu(window);
+        if (menu_navigation[0] == MAIN_MENU) {
+            next_component = main_menu.run_menu(window);
+            if (next_component != MAIN_MENU) {
+                string menu_navigation[] = { next_component, MAIN_MENU };
+            }
         }
-        else if (current_component == LEVEL_ONE) {
-            current_component = level_one(window);
+        else if (menu_navigation[0] == LEVEL_ONE) {
+            next_component = level_one.run_menu(window);
+            if (next_component != LEVEL_ONE) {
+                string menu_navigation[] = { next_component, LEVEL_ONE };
+            }
         }
-        else if (current_component == LEVEL_TWO) {
-            current_component = level_two(window);
+        else if (menu_navigation[0] == LEVEL_TWO) {
+            next_component = level_two.run_menu(window);
+            if (next_component != LEVEL_ONE) {
+                string menu_navigation[] = { next_component, LEVEL_TWO };
+            }
         }
-        else if (current_component == END_MENU) {
-            current_component = end_menu(window);
+        else if (menu_navigation[0] == END_MENU) {
+            next_component = end_menu.run_menu(window);
+            if (next_component != END_MENU) {
+                string menu_navigation[] = { next_component, END_MENU };
+            }
         }
         else {
             cout << "Current level not recognised!!!";
-            current_component = MAIN_MENU;
+            menu_navigation[0] = MAIN_MENU;
+            menu_navigation[1] = MAIN_MENU;
         }
 
         window.display();
