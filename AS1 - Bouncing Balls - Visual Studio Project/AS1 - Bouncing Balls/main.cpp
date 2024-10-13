c2a1e1a4f82b000021edb5040e6c6f577d7b7525
Thomas Jebson <ThomasJebbo@gmail.com>
2024-10-13 23:22:53 +0100

    I have literally no idea whats causing this issue....

--- AS1 - Bouncing Balls - Visual Studio Project/AS1 - Bouncing Balls/main.cpp
+++ AS1 - Bouncing Balls - Visual Studio Project/AS1 - Bouncing Balls/main.cpp
@@ -1,18 +1,41 @@
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <windows.h>
 
 #define MAIN_MENU "main menu"
 #define LEVEL_ONE "level one"
 #define LEVEL_TWO "level two"
 #define END_MENU "end menu"
+#define PAUSE_MENU "pause menu"
 #define WINDOW_SIZE {1000, 800} // Actual Game Size {600, 400}
 
-#define ENTER_KEY 58
-#define ESCAPE_KEY 36
+#define FONT_DIRECTORY "G:\\University Assignments\\CSY1079 - Programming 2D Computer Games\\AS1-Bouncing-Balls\\AS1 - Bouncing Balls - Visual Studio Project\\fonts"
+#define NOTO_SANS_JP_FONT_PATH FONT_DIRECTORY + "\\Noto_Sans JP\\NotoSansJP - Regular.ttf"
 
 using namespace std;
+
+string PROJECT_RESOURCES_DIRECTORY = "G:\\University Assignments\\CSY1079 - Programming 2D Computer Games\\AS1-Bouncing-Balls\\AS1 - Bouncing Balls - Visual Studio Project";
+
+string path_builder(string (&args)[50]) {
+    string resultant_path = PROJECT_RESOURCES_DIRECTORY;
+    int length_of_input_array = sizeof(args) / sizeof(args[0]);
+    for (int i = 0; i < length_of_input_array; i++) {
+        if (args[i] == "") break;
+        resultant_path += "\\" + args[i];
+    }
+    return resultant_path;
+}
+
+void play_random_pop_sounds(sf::Music* pop_sounds, int count) {
+    for (int i = 0; i < count; i++) {
+        int sound_index = rand() % 15;
+        pop_sounds[sound_index].play();
+    }
+}
 
 sf::Color pick_ball_color() {
     int randomly_chosen_color = rand() % 3;
@@ -76,15 +99,47 @@ public:
 };
 
 class MainMenu {
+    int rotation;
+    string game_rules = "AIM AT BUBBLES OF THE SAME COLOR AND SHOOT!";
 public:
     MainMenu() {
+        rotation = (rand() % 90)-45;
     }
 
     string run_menu(sf::RenderWindow& window) {
+        sf::Text text;
+        sf::Font font;
+        string path_components[50] = { "Fonts", "Noto_Sans JP", "NotoSansJP-Regular.ttf" };
+        font.loadFromFile(path_builder(path_components));
+
+        // select the font
+        text.setFont(font); // font is a sf::Font
+
+        // set the string to display
+        text.setString(L"パズルボブル");
+
+        // set the character size
+        text.setCharacterSize(300); // in pixels, not points!
+           
+        // set text rotation
+        text.rotate(rotation);
+
+        // get text size
+        int text_x_size = text.getLocalBounds().width;
+        int text_y_size = text.getLocalBounds().height;
+
+        // set text position
+        text.setPosition(500, 500);
+
+        // set the color
+        text.setFillColor(sf::Color(225, 225, 225));
+
         int score = 0;
         sf::CircleShape shape(100.f);
         shape.setFillColor(pick_ball_color());
+        shape.setPosition({ 100, 200 });
         window.draw(shape);
+        window.draw(text);
         return MAIN_MENU;
     }
 
@@ -99,6 +154,9 @@ public:
         return LEVEL_ONE;
     }
 
+    void reset_level() {
+    }
+
 };
 
 class LevelTwo {
@@ -108,6 +166,9 @@ public:
 
     string run_menu(sf::RenderWindow& window) {
         return LEVEL_TWO;
+    }
+
+    void reset_level() {
     }
 };
 
@@ -121,8 +182,21 @@ public:
     }
 };
 
+class PauseMenu {
+public:
+    PauseMenu() {
+    }
+
+    string run_menu(sf::RenderWindow& window, string *menu_navigation) {
+        return menu_navigation[1]; // go to previous level
+        return PAUSE_MENU; // stay on current window alternate between these for menu navigation
+    }
+};
+
 int main()
 {
+    srand(time(0)); // enforces random values using seed of current time.
+
     // setup local variables
     string menu_navigation[] = { MAIN_MENU, MAIN_MENU };
     /*
@@ -144,11 +218,25 @@ int main()
     LevelOne level_one = LevelOne();
     LevelTwo level_two = LevelTwo();
     EndMenu end_menu = EndMenu();
+    PauseMenu pause_menu = PauseMenu();
 
     // create window (do last so not unresponsive whilst the game loads)
     sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE), "Bouncing Balls!");
     window.setVerticalSyncEnabled(true); // vsync is our frame limiter here.
     // No need for clocks!
+
+    // Load pop sounds. Using music here means files arent actually loaded
+    // and instead streamed as needed, meaning this isnt actually that
+    // inefficient!
+    sf::Music pop_sounds[15];
+    for (int i = 0; i < 15; i++) { // 16
+        string file = to_string(i+1) + ".wav";
+        string path_components[50] = { "resources", "sounds", "pops", file };
+        string file_path = path_builder(path_components);
+        pop_sounds[i].openFromFile(file_path);
+    }
+
+    play_random_pop_sounds(pop_sounds, 13);
 
     while (window.isOpen())
     {
@@ -159,17 +247,17 @@ int main()
                 window.close();
                 break;
             } else if(event.type == sf::Event::KeyPressed) {
-                if (event.key.code == ENTER_KEY) {
+                if (event.key.code == sf::Keyboard::Enter) {
                     mouse.set_left_button_pressed(true);
                 }
-                else if (event.key.code == ESCAPE_KEY) {
+                else if (event.key.code == sf::Keyboard::Escape) {
                     string current_menu = menu_navigation[0];
                     string menu_navigation[] = { MAIN_MENU, current_menu };
                 }
             }
         }
 
-        window.clear();
+        window.clear(sf::Color(255, 255, 255));
 
         if (menu_navigation[0] == MAIN_MENU) {
             next_component = main_menu.run_menu(window);
@@ -181,18 +269,30 @@ int main()
             next_component = level_one.run_menu(window);
             if (next_component != LEVEL_ONE) {
                 string menu_navigation[] = { next_component, LEVEL_ONE };
+                if (next_component != PAUSE_MENU) {
+                    level_two.reset_level();
+                }
             }
         }
         else if (menu_navigation[0] == LEVEL_TWO) {
             next_component = level_two.run_menu(window);
             if (next_component != LEVEL_ONE) {
                 string menu_navigation[] = { next_component, LEVEL_TWO };
+                if (next_component != PAUSE_MENU) {
+                    level_two.reset_level();
+                }
             }
         }
         else if (menu_navigation[0] == END_MENU) {
             next_component = end_menu.run_menu(window);
             if (next_component != END_MENU) {
                 string menu_navigation[] = { next_component, END_MENU };
+            }
+        }
+        else if (menu_navigation[0] == PAUSE_MENU) {
+            next_component = pause_menu.run_menu(window, menu_navigation);
+            if (next_component != PAUSE_MENU) {
+                string menu_navigation[] = { next_component, PAUSE_MENU };
             }
         }
         else {
