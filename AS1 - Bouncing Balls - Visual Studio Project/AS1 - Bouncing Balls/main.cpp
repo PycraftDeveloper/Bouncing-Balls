b6ccba84eb74c4b342b3aef0ae225c49ed69b5e2
Thomas Jebson <ThomasJebbo@gmail.com>
2024-10-20 14:31:08 +0100

    major changes
    introduced ball with collisions, mass and varying quality!

--- AS1 - Bouncing Balls - Visual Studio Project/AS1 - Bouncing Balls/main.cpp
+++ AS1 - Bouncing Balls - Visual Studio Project/AS1 - Bouncing Balls/main.cpp
@@ -11,14 +11,36 @@
 #define LEVEL_TWO "level two"
 #define END_MENU "end menu"
 #define PAUSE_MENU "pause menu"
-#define WINDOW_SIZE {1000, 800} // Actual Game Size {600, 400}
 
 #define FONT_DIRECTORY "G:\\University Assignments\\CSY1079 - Programming 2D Computer Games\\AS1-Bouncing-Balls\\AS1 - Bouncing Balls - Visual Studio Project\\fonts"
 #define NOTO_SANS_JP_FONT_PATH FONT_DIRECTORY + "\\Noto_Sans JP\\NotoSansJP - Regular.ttf"
 
 using namespace std;
 
-string PROJECT_RESOURCES_DIRECTORY = "G:\\University Assignments\\CSY1079 - Programming 2D Computer Games\\AS1-Bouncing-Balls\\AS1 - Bouncing Balls - Visual Studio Project";
+// define global CONSTANTS
+string PROJECT_RESOURCES_DIRECTORY = "G:\\University\\Assignments\\Simester 1\\CSY1079 - Programming 2D Computer Games\\AS1 - Bouncing Balls - Visual Studio Project";
+const float TAU = 3.14159265358979323 * 2;
+
+// define global VARIABLES
+int window_size[] = { 1280, 720 };
+
+float pythagorean_distance(float x1, float y1, float x2, float y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+int calculate_point_count(int radius, float shape_quality = 0.75) {
+    if (radius < 1) {
+        return 3;
+    }
+    float angle = asin(1.0f / radius);
+    int point_count = 1 + int((TAU / angle) * shape_quality);
+
+    if (point_count < 3) {
+        point_count = 3;
+    }
+
+    return point_count;
+}
 
 string path_builder(string (&args)[50]) {
     string resultant_path = PROJECT_RESOURCES_DIRECTORY;
@@ -37,8 +59,8 @@ void play_random_pop_sounds(sf::Music* pop_sounds, int count) {
         int sound_index = rand() % 15;
         pop_sounds[sound_index].play();
     }
-    // it is possible to do this serially. By creating custom music objects with a play function that mimmiks the music.play function and then updating these objects every frame with custom delays.
-    // Note: doing this would require that the audio isnt already in its delay state which could easily be added in. This limits max number of sounds to 15, as thats how many unique files we have,
+    // it is possible to do this serially. By creating custom music objects with a play function that mimics the music.play function and then updating these objects every frame with custom delays.
+    // Note: doing this would require that the audio isn't already in its delay state which could easily be added in. This limits max number of sounds to 15, as that's how many unique files we have,
     // however clever sound design should be able to mask this limitation.
 }
 
@@ -56,6 +78,96 @@ sf::Color pick_ball_color() {
 }
 
 class Ball {
+public:
+    float x, y;
+    float mass;
+    sf::Color color;
+    float x_speed, y_speed;
+    sf::CircleShape shape;
+    bool has_drag;
+    bool can_bounce;
+
+    Ball(float x, float y, float mass, sf::Color color, bool has_drag, bool can_bounce) : x(x), y(y), mass(mass), color(color), has_drag(has_drag), can_bounce(can_bounce) {
+        x_speed = (rand() % 15) + 1;
+        y_speed = (rand() % 15) + 1;
+
+        shape.setFillColor(color);
+        shape.setRadius(mass);
+        shape.setOrigin(mass, mass); // Centre the origin for proper positioning
+        shape.setPointCount(calculate_point_count(mass));
+    }
+
+    void compute() {
+        x += x_speed;
+        y += y_speed;
+
+        // Correct the boundary collision to respect ball's radius
+        if ((x - mass <= 0 && x_speed < 0) || (x + mass >= window_size[0] && x_speed > 0)) {
+            x_speed = -x_speed;
+        }
+        if ((y - mass <= 0 && y_speed < 0) || (y + mass >= window_size[1] && y_speed > 0)) {
+            y_speed = -y_speed;
+        }
+
+        // Keep the ball inside the window
+        if (x - mass < 0) x = mass;
+        if (x + mass > window_size[0]) x = window_size[0] - mass;
+        if (y - mass < 0) y = mass;
+        if (y + mass > window_size[1]) y = window_size[1] - mass;
+
+        if (has_drag) {
+            x_speed *= 0.99;
+            y_speed *= 0.99;
+        }
+    }
+
+    void collision(Ball& ball) {
+        // Calculate the distance between the two balls
+        float distance = pythagorean_distance(x, y, ball.x, ball.y);
+        if (distance < mass + ball.mass) {
+            // Calculate the angle of the collision
+            float angle = atan2(y - ball.y, x - ball.x);
+            float sin_angle = sin(angle);
+            float cos_angle = cos(angle);
+
+            // Move the balls apart to prevent overlap
+            float overlap = 0.5f * (mass + ball.mass - distance + 1); // Slight overlap prevention
+            x += cos_angle * overlap;
+            y += sin_angle * overlap;
+            ball.x -= cos_angle * overlap;
+            ball.y -= sin_angle * overlap;
+
+            if (can_bounce) {
+                // Rotate the velocities so that the collision is head-on
+                float v1_x_rot = cos_angle * x_speed + sin_angle * y_speed;
+                float v1_y_rot = cos_angle * y_speed - sin_angle * x_speed;
+                float v2_x_rot = cos_angle * ball.x_speed + sin_angle * ball.y_speed;
+                float v2_y_rot = cos_angle * ball.y_speed - sin_angle * ball.x_speed;
+
+                // Apply the 1D collision equations along the rotated axis
+                float v1_x_rot_new = (v1_x_rot * (mass - ball.mass) + 2 * ball.mass * v2_x_rot) / (mass + ball.mass);
+                float v2_x_rot_new = (v2_x_rot * (ball.mass - mass) + 2 * mass * v1_x_rot) / (mass + ball.mass);
+
+                // Apply component velocities to ball
+                x_speed = cos_angle * v1_x_rot_new - sin_angle * v1_y_rot;
+                y_speed = sin_angle * v1_x_rot_new + cos_angle * v1_y_rot;
+                ball.x_speed = cos_angle * v2_x_rot_new - sin_angle * v2_y_rot;
+                ball.y_speed = sin_angle * v2_x_rot_new + cos_angle * v2_y_rot;
+            }
+            else {
+                // Apply component velocities to ball
+                x_speed = 0;
+                y_speed = 0;
+                ball.x_speed = 0;
+                ball.y_speed = 0;
+            }
+        }
+    }
+
+    void draw(sf::RenderWindow& window) {
+        shape.setPosition(x, y);
+        window.draw(shape);
+    }
 };
 
 class Cannon {
@@ -226,7 +338,7 @@ int main()
     PauseMenu pause_menu = PauseMenu();
 
     // create window (do last so not unresponsive whilst the game loads)
-    sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE), "Bouncing Balls!");
+    sf::RenderWindow window(sf::VideoMode(window_size[0], window_size[1]), "Bouncing Balls!");
     window.setVerticalSyncEnabled(true); // vsync is our frame limiter here.
     // No need for clocks!
 
